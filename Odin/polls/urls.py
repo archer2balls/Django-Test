aebd0a7eacec86e802749ca240ce7e4797bea105
Michael P <38262473+archer2balls@users.noreply.github.com>
2020-12-15 12:44:27 +0000

    Update urls.py

--- Odin/polls/urls.py
+++ Odin/polls/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('members/', views.members, name='members'),
+    path('members/', views.members, name='members'),
 ]
