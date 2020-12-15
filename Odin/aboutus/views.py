282b21f15803eb6deda24a7e9c2c3237159e8468
archer2balls <38262473+archer2balls@users.noreply.github.com>
2020-12-15 20:10:48 +0000

    Added date time to page

--- Odin/aboutus/views.py
+++ Odin/aboutus/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("<h1>Hello, world. This is ABOUT US!!!</h1>")
+    curDT = datetime.now()
+    date_time = curDT.strftime("%d/%m/%Y, %H:%M:%S") # Can set date it as m/d/Y also
+    return render(request,"aboutus/index.html",{"datetime":date_time})
+    #return HttpResponse("<h1>Hello, world. This is ABOUT US!!!</h1>")
+
 
 def aboutus(request):
     return HttpResponse("<h1>Hello, world. This is about us!!!</h1>")
