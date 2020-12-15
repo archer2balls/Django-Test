a140222dfdf6671da6b47ed180de16ca5867c317
Michael P <38262473+archer2balls@users.noreply.github.com>
2020-12-15 12:44:00 +0000

    Update views.py

--- Odin/polls/views.py
+++ Odin/polls/views.py
@@ -7,3 +7,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>Hello, world. You're at the polls index</h1>")
 
+def members(request):
+    return HttpResponse("<h1>Members Area Only</h1>")
