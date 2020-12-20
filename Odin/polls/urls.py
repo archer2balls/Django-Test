1a31b12d29a7e835311de2751d6ac69339b111d0
archer2balls <38262473+archer2balls@users.noreply.github.com>
2020-12-20 10:26:28 +0000

    added views

--- Odin/polls/urls.py
+++ Odin/polls/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # eg: /polls/
     path('', views.index, name='index'),
+    # eg: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # eg: /polls/5/results
+    path('<int:question_id>/results', views.results, name='results'),
+    # eg: /polls/5/vote
+    path('<int:question_id>/vote', views.vote, name='vote'),
     path('members/', views.members, name='members'),
+
 ]
--- Odin/polls/views.py
+++ Odin/polls/views.py
@@ -2,10 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, response
+from .models import Question
 
 def index(request):
-    return HttpResponse("<h1>Hello, world. You're at the Polls!!!</h1>")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+# Leave the rest of the views (detail, results, vote) unchanged.
+
+
+# def index(request):
+#     return HttpResponse("<h1>Hello, world. You're at the Polls!!!</h1>")
 
 def members(request):
     return HttpResponse("<h1>Members Area Only</h1>")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
